4a77464cbb3a033c5286c415d250fbd0dbe2ef9d
vanshgupta11 <your-email@example.com>
2026-04-09 11:26:39 +0530

    build: make app deployment ready with env variables and vercel config

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,8 +8,8 @@ const cors = require("cors")
 const app = express();
 
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true
 }));
 app.use(express.json())
 app.use(cookieParser())
